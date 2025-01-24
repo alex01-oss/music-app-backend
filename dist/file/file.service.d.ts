80352473e810d865c5d891f7a3304d6dab9bd8d6
Александр Демочко <alexa.demochko@gmail.com>
2025-01-24 18:22:26 +0200

    Add .gitignore and clean up ignored files

--- dist/file/file.service.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum FileType {
-    AUDIO = "audio",
-    IMAGE = "image"
-}
-export declare class FileService {
-    createFile(type: FileType, file: Express.Multer.File): string;
-    removeFile(fileName: string): void;
-}
--- dist/track/dto/create-comment.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { Types } from "mongoose";
-export declare class CreateCommentDto {
-    readonly username: string;
-    readonly text: string;
-    readonly trackId: Types.ObjectId;
-}
